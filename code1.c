0917f59d4f8e071320572b92e8ac621a66905648
Khang Phan <khangphan0515@gmail.com>
2024-10-13 15:52:01 -0500

    code1 version 0.2

--- code1.c
+++ code1.c
@@ -10,12 +10,30 @@ Description: Given 12 monthly sales figures in an input file, generates monthly 
 #include <string.h>
 #include <stdlib.h>
 
+void monthly_sales_report(float months[]) {
+    printf("Monthly Sales Report for 2024");
+    printf("Month\t\tSales");
+    printf("January\t\t%0.2f\n", months[0]);
+    printf("February\t%0.2f\n", months[1]);
+    printf("March\t\t%0.2f\n", months[2]);
+    printf("April\t\t%0.2f\n", months[3]);
+    printf("May\t\t%0.2f\n", months[4]);
+    printf("June\t\t%0.2f\n", months[5]);
+    printf("July\t\t%0.2f\n", months[6]);
+    printf("August\t\t%0.2f\n", months[7]);
+    printf("September\t%0.2f\n", months[8]);
+    printf("October\t\t%0.2f\n", months[9]);
+    printf("November\t%0.2f\n", months[10]);
+    printf("December\t%0.2f\n", months[11]);
+}
+
 int main() {
-    char fname[100], line[256];
-    int month=1;
+    char file[100];
+    float months[12], sales;
+    int x=0;
     printf("Enter file name: ");
-    scanf("%s", &fname);
-    FILE *fp = fopen(fname, "r");
+    scanf("%s", &file);
+    FILE *fp = fopen(file, "r");
 
     if (fp == NULL) {
         printf("Error opening file.\n");
@@ -23,47 +41,12 @@ int main() {
     }
 
     printf("Monthly Sales Report for 2024\nMonth\t\tSales\n");
-    while (fgets(line, 256, fp) != NULL) {
-        switch (month) {
-            case 1:
-                printf("January\t\t%s", line);
-                break;
-            case 2:
-                printf("February\t%s", line);
-                break;
-            case 3:
-                printf("March\t\t%s", line);
-                break;
-            case 4:
-                printf("April\t\t%s", line);
-                break;
-            case 5:
-                printf("May\t\t%s", line);
-                break;
-            case 6:
-                printf("June\t\t%s", line);
-                break;
-            case 7:
-                printf("July\t\t%s", line);
-                break;
-            case 8:
-                printf("August\t\t%s", line);
-                break;
-            case 9:
-                printf("September\t%s", line);
-                break;
-            case 10:
-                printf("October\t\t%s", line);
-                break;
-            case 11:
-                printf("November\t%s", line);
-                break;
-            case 12:
-                printf("December\t%s", line);
-                break;
-        }
-        month++;
+    while (fscanf(fp, "%f", &sales) != EOF) {
+        months[x] = sales;
+        x++;
     }
+
+    printf("Sales Summary Report:\n");
 
     fclose(fp);
 
