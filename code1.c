571cc5b57b0b50e8e83d3f1f0c58a0c22a36ee57
Khang Phan <khangphan0515@gmail.com>
2024-10-13 19:43:40 -0500

    code1 added comments

--- code1.c
+++ code1.c
@@ -11,15 +11,19 @@ Description: Given 12 monthly sales figures in an input file, generates monthly 
 #include <stdlib.h>
 
 struct monthlySales {
+    //structure of month names and sales numbers
+    //used only for sales report (highest to lowest) and later sales summary report
     char month[9];
     float sales;
 };
 
 int compare(const void *a, const void *b) {
+    //compares sales values of two months and swaps depending on relationship
     return ((struct monthlySales*)b)->sales - ((struct monthlySales*)a)->sales;
 }
 
 void monthly_sales_report(float months[]) {
+    //prints each month out with monthly sales rounded to second decimal place
     printf("\nMonthly Sales Report for 2024\n\n");
     printf("Month\t\tSales\n");
     printf("January\t\t%0.2f\n", months[0]);
@@ -37,11 +41,14 @@ void monthly_sales_report(float months[]) {
 }
 
 void sales_summary_report(float months[]) {
+    //creates array of monthly sales called yearly
     struct monthlySales yearly[12];
     float avg;
     for (int i=0; i<12; i++) {
+        //sets each monthly sales' sales value to corresponding array months' value
         yearly[i].sales = months[i];
     }
+    //month names are done separately
     strcpy(yearly[0].month, "January");
     strcpy(yearly[1].month, "February");
     strcpy(yearly[2].month, "March");
@@ -57,24 +64,27 @@ void sales_summary_report(float months[]) {
 
     struct monthlySales min = yearly[0], max = yearly[0];
     for (int i=0; i<12; i++) {
+        //iterates through each monthly sale, changing max and min to the greatest/least values
         if (yearly[i].sales > max.sales) {
             max = yearly[i];
         }
         if (yearly[i].sales < min.sales) {
             min = yearly[i];
         }
-        avg += months[i];
+        avg += months[i];   //adds each month's sales together
     }
     printf("\nSales Summary Report:\n\n");
     printf("Minimum Sales: %0.2f (%s)\n", min.sales, min.month);
     printf("Maximum Sales: %0.2f (%s)\n", max.sales, max.month);
-    printf("Average Sales: %0.2f\n", avg/12);
+    printf("Average Sales: %0.2f\n", avg/12);   //divides total of sales by 12 to get average
 }
 
 void six_month_moving_average_report(float months[]) {
+    //sets j as the upper bound at 6
     int j=6;
     float avg, sixavg[7];
     while (j<=12) {
+        //same process as before but now puts 6-month averages into an array
         avg = 0;
         for (int i=j-6; i<j; i++) {
             avg += months[i];
@@ -82,6 +92,7 @@ void six_month_moving_average_report(float months[]) {
         sixavg[j-6] = avg;
         j++;
     }
+    //prints results from the array
     printf("\nSix Month moving average report:\n\n");
     printf("January June\t%0.2f\n", sixavg[0]/6);
     printf("February July\t%0.2f\n", sixavg[1]/6);
@@ -93,6 +104,7 @@ void six_month_moving_average_report(float months[]) {
 }
 
 void sales_report_high_low(float months[]) {
+    //creates another array of structures
     struct monthlySales yearly[12];
     for (int i=0; i<12; i++) {
         yearly[i].sales = months[i];
@@ -110,15 +122,18 @@ void sales_report_high_low(float months[]) {
     strcpy(yearly[10].month, "November\t");
     strcpy(yearly[11].month, "December\t");
     
+    //sorts the structures from greatest to least in terms of sales
     qsort(yearly, 12, sizeof(yearly[0]), compare);
     printf("\nSales report (highest to lowest) :\n\n");
     printf("Month\t\tSales\n");
     for (int i=0; i<12; i++) {
+        //prints out each month's sales with the month name in descending order of sales
         printf("%s$%0.2f\n", yearly[i].month, yearly[i].sales);
     }
 }
 
 int main() {
+    //allows file name up to 100 characters, create array of 12 months' sales values
     char file[100];
     float months[12], sales;
     int x=0;
@@ -127,11 +142,13 @@ int main() {
     FILE *fp = fopen(file, "r");
 
     if (fp == NULL) {
+        //checks for valid file opened
         printf("Error opening file.\n");
-        return 1; 
+        return 1; //indicates error
     }
 
     while (fscanf(fp, "%f", &sales) != EOF) {
+        //scans each line of floats and puts them into the array
         months[x] = sales;
         x++;
     }
