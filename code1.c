093af0f9e85fb4f12fae44848daeaaede6423ddf
Khang Phan <khangphan0515@gmail.com>
2024-10-13 19:21:21 -0500

    code1 version 1.0

--- code1.c
+++ code1.c
@@ -10,8 +10,17 @@ Description: Given 12 monthly sales figures in an input file, generates monthly 
 #include <string.h>
 #include <stdlib.h>
 
+struct monthlySales {
+    char month[9];
+    float sales;
+};
+
+int compare(const void *a, const void *b) {
+    return ((struct monthlySales*)b)->sales - ((struct monthlySales*)a)->sales;
+}
+
 void monthly_sales_report(float months[]) {
-    printf("Monthly Sales Report for 2024\n\n");
+    printf("\nMonthly Sales Report for 2024\n\n");
     printf("Month\t\tSales\n");
     printf("January\t\t%0.2f\n", months[0]);
     printf("February\t%0.2f\n", months[1]);
@@ -28,19 +37,37 @@ void monthly_sales_report(float months[]) {
 }
 
 void sales_summary_report(float months[]) {
-    float min=months[0], max=months[0], avg;
+    struct monthlySales yearly[12];
+    float avg;
     for (int i=0; i<12; i++) {
-        if (months[i] > max) {
-            max = months[i];
+        yearly[i].sales = months[i];
+    }
+    strcpy(yearly[0].month, "January");
+    strcpy(yearly[1].month, "February");
+    strcpy(yearly[2].month, "March");
+    strcpy(yearly[3].month, "April");
+    strcpy(yearly[4].month, "May");
+    strcpy(yearly[5].month, "June");
+    strcpy(yearly[6].month, "July");
+    strcpy(yearly[7].month, "August");
+    strcpy(yearly[8].month, "September");
+    strcpy(yearly[9].month, "October");
+    strcpy(yearly[10].month, "November");
+    strcpy(yearly[11].month, "December");
+
+    struct monthlySales min = yearly[0], max = yearly[0];
+    for (int i=0; i<12; i++) {
+        if (yearly[i].sales > max.sales) {
+            max = yearly[i];
         }
-        if (months[i] < min) {
-            min = months[i];
+        if (yearly[i].sales < min.sales) {
+            min = yearly[i];
         }
         avg += months[i];
     }
     printf("\nSales Summary Report:\n\n");
-    printf("Minimum Sales: %0.2f\n", min);
-    printf("Maximum Sales: %0.2f\n", max);
+    printf("Minimum Sales: %0.2f (%s)\n", min.sales, min.month);
+    printf("Maximum Sales: %0.2f (%s)\n", max.sales, max.month);
     printf("Average Sales: %0.2f\n", avg/12);
 }
 
@@ -50,14 +77,12 @@ void six_month_moving_average_report(float months[]) {
     while (j<=12) {
         avg = 0;
         for (int i=j-6; i<j; i++) {
-            printf("%f\n", months[i]);
             avg += months[i];
         }
-        printf("separator!!!!!\n");
         sixavg[j-6] = avg;
         j++;
     }
-    printf("Six Month moving average report:\n\n");
+    printf("\nSix Month moving average report:\n\n");
     printf("January June\t%0.2f\n", sixavg[0]/6);
     printf("February July\t%0.2f\n", sixavg[1]/6);
     printf("March August\t%0.2f\n", sixavg[2]/6);
@@ -65,6 +90,32 @@ void six_month_moving_average_report(float months[]) {
     printf("May October\t%0.2f\n", sixavg[4]/6);
     printf("June November\t%0.2f\n", sixavg[5]/6);
     printf("July December\t%0.2f\n", sixavg[6]/6);
+}
+
+void sales_report_high_low(float months[]) {
+    struct monthlySales yearly[12];
+    for (int i=0; i<12; i++) {
+        yearly[i].sales = months[i];
+    }
+    strcpy(yearly[0].month, "January\t\t");
+    strcpy(yearly[1].month, "February\t");
+    strcpy(yearly[2].month, "March\t\t");
+    strcpy(yearly[3].month, "April\t\t");
+    strcpy(yearly[4].month, "May\t\t");
+    strcpy(yearly[5].month, "June\t\t");
+    strcpy(yearly[6].month, "July\t\t");
+    strcpy(yearly[7].month, "August\t\t");
+    strcpy(yearly[8].month, "September\t");
+    strcpy(yearly[9].month, "October\t\t");
+    strcpy(yearly[10].month, "November\t");
+    strcpy(yearly[11].month, "December\t");
+    
+    qsort(yearly, 12, sizeof(yearly[0]), compare);
+    printf("\nSales report (highest to lowest) :\n\n");
+    printf("Month\t\tSales\n");
+    for (int i=0; i<12; i++) {
+        printf("%s$%0.2f\n", yearly[i].month, yearly[i].sales);
+    }
 }
 
 int main() {
@@ -88,6 +139,7 @@ int main() {
     monthly_sales_report(months);
     sales_summary_report(months);
     six_month_moving_average_report(months);
+    sales_report_high_low(months);
 
     fclose(fp);
 
